dced134abb1aa0074abf100f1a9fdc505b6a3585
thecaptaincarrot <trevorlandersen@gmail.com>
2023-06-25 23:45:14 -0500

    Dataaaaaa

--- Resources/Classes/Data_Class.gd
+++ Resources/Classes/Data_Class.gd
@@ -1,7 +1,19 @@
-extends Global_Resource
+extends Resource
 class_name Data
 
 @export var DataType = "DEFAULT"
-
 @export var DataColor : Color
 
+var value = 0.0
+
+
+func get_value():
+	return value
+
+
+func add_value(amount):
+	value += amount
+
+
+func spend(amount):
+	value -= amount
