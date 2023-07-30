4ae81d63a60ea65706f657867dd6f6ad801d1e86
thecaptaincarrot <trevorlandersen@gmail.com>
2023-07-30 16:32:05 -0500

    Can Stage Rocket Designs

--- UI_Components/Design/RightDesignPanel.gd
+++ UI_Components/Design/RightDesignPanel.gd
@@ -15,22 +15,28 @@ func _process(delta):
 func _on_rocket_builder_rocket_changed(modules_list):
 	print(modules_list)
 	var total_mass = 0
-	var total_thrust = 0
-	var total_mpd = 0 #Money per day
-	var total_money_cost = 0 #money
-	var total_ore_cost = 0
+	var total_cost = {}
 	
 	for module in modules_list:
 		total_mass += module.mass
-		total_money_cost += module.money_cost #money
-		total_ore_cost += module.ore_cost
+		for cost_key in module.costs.keys():
+			if total_cost.keys().has(cost_key):
+				total_cost[cost_key] += module.costs[cost_key]
+			else:
+				total_cost[cost_key] = module.costs[cost_key]
 	
 	#Set our labels
 	$MarginContainer/VBoxContainer/MassBox/Mass.text = str(total_mass)
-	$MarginContainer/VBoxContainer/ThrustBox/Thrust.text = str(total_thrust)
-	$MarginContainer/VBoxContainer/MPDBox/MoneyPerDay.text = str(total_mpd)
-	$MarginContainer/VBoxContainer/MoneyBox/MoneyCost.text = str(total_money_cost)
-	$MarginContainer/VBoxContainer/OreBox/OreCost.text = str(total_ore_cost)
+	#Costs
+	for N in $MarginContainer/VBoxContainer/CostContainer.get_children():
+		if N.name != "COST":
+			N.queue_free()
+	
+	for cost_key in total_cost.keys():
+		if total_cost[cost_key] > 0:
+			var new_label = Label.new()
+			new_label.text = cost_key + ": " + str(total_cost[cost_key])
+			$MarginContainer/VBoxContainer/CostContainer.add_child(new_label)
 
 
 func _on_h_slider_value_changed(value):
