5cd9ce1b1fc2647955abd9e0568ca36bff61752f
thecaptaincarrot <trevorlandersen@gmail.com>
2023-08-06 16:38:40 -0500

    and nothing of consequence was done

--- Classes/Research/LaunchTech.gd
+++ Classes/Research/LaunchTech.gd
@@ -4,5 +4,9 @@ class_name LaunchTechnology
 @export var exhaust_velocity = 0.0
 
 #per_stage
-@export var dry_mass = 0.0 #Kg
-@export var max_fuel_mass = 0.0
+@export var dry_mass = 0.0 #Kg, including engines and shit
+@export var max_fuel_mass = 0.0 #kg, per stage?
+
+@export var maximum_stages = 1
+
+
